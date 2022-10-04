283e98d8b5119dd76c3a6e554288651aad7663ca
theresejoseph <55788959+theresejoseph@users.noreply.github.com>
2022-10-04 17:02:39 +1000

    kitti processing

--- scripts/MultiScale/Multiscale_1D_Decoding.py
+++ scripts/MultiScale/Multiscale_1D_Decoding.py
@@ -17,7 +17,7 @@ from CAN import  attractorNetworkSettling, attractorNetwork, attractorNetworkSca
 
 def visualiseMultiResolution1D(velocities,scale,velocity_type, visualise=False):
     global prev_weights, num_links, excite, curr_parameter
-    N=80
+    N=100
     # num_links,excite,activity_mag,inhibit_scale=6,3,1.01078180e-01,8.42457941e-01
     num_links,excite,activity_mag,inhibit_scale=3,3,0.6923,0.0064
 
@@ -28,9 +28,9 @@ def visualiseMultiResolution1D(velocities,scale,velocity_type, visualise=False):
 
     num_links,excite,activity_mag,inhibit_scale=1,1,4.33654032e-02,8.51548180e-02
 
-    num_links,excite,activity_mag,inhibit_scale=1,3,1.21745813e-01,9.96673372e-02
+    num_links,excite,activity_mag,inhibit_scale=1,3,1.21745813e-01,5.96673372e-02
 
-    num_links,excite,activity_mag,inhibit_scale=1,3,0.0521745813,5.96673372e-02
+    num_links,excite,activity_mag,inhibit_scale=1,3,0.0721745813,2.96673372e-02
 
 
     # num_links,excite,activity_mag,inhibit_scale=1,4,1.00221581e-01,1.29876096e-01
@@ -151,12 +151,11 @@ def visualiseMultiResolution1D(velocities,scale,velocity_type, visualise=False):
     plt.show()
 
 
-
 def visualiseMultiResolution1DLandmark(velocities_1rep,scale,visulaise=False):
     global prev_weights, num_links, excite, curr_parameter
     N=240
-    # velocities=np.concatenate([velocities_1rep,velocities_1rep])
-    velocities=velocities_1rep
+    velocities=np.concatenate([velocities_1rep,velocities_1rep])
+    # velocities=velocities_1rep
 
     # num_links,excite,activity_mag,inhibit_scale=6,3,1.01078180e-01,8.42457941e-01
 
@@ -168,6 +167,7 @@ def visualiseMultiResolution1DLandmark(velocities_1rep,scale,visulaise=False):
     num_links,excite,activity_mag,inhibit_scale=9,1,2.07046267e-03,4.45269855e-02
 
     num_links,excite,activity_mag,inhibit_scale=1,3,0.221745813,0.0496673372
+    num_links,excite,activity_mag,inhibit_scale=1,3,0.0721745813,2.96673372e-02
     
     integratedPos=[0]
     decodedPos=[0]
@@ -257,34 +257,34 @@ def visualiseMultiResolution1DLandmark(velocities_1rep,scale,visulaise=False):
 
             print(f"{str(i)}  translation {input} input output {round(integratedPos[-1],3)}  {str(decoded_translation )}  ")
             
-            # ax10.set_title(str(scale[0])+" Scale",fontsize=9)
-            # ax11.set_title(str(scale[1])+" Scale",fontsize=9)
-            # ax12.set_title(str(scale[2])+" Scale",fontsize=9)
-            # ax13.set_title(str(scale[3])+" Scale",fontsize=9)
-            # ax14.set_title(str(scale[4])+" Scale",fontsize=9)
+            ax10.set_title(str(scale[0])+" Scale",fontsize=9)
+            ax11.set_title(str(scale[1])+" Scale",fontsize=9)
+            ax12.set_title(str(scale[2])+" Scale",fontsize=9)
+            ax13.set_title(str(scale[3])+" Scale",fontsize=9)
+            ax14.set_title(str(scale[4])+" Scale",fontsize=9)
             
             axtxt1.text(0,0.5,f" Shift: {round(input,4)}, Decoded Trans: {round(decoded_translation,3)}", c='r')
             axtxt1.text(0,0,"Decoded Position of Each Network: " + str(split_output), c='r')
 
-            # ax10.bar(np.arange(N),prev_weights[0][:],color='aqua')
-            # ax10.get_xaxis().set_visible(False)
-            # ax10.spines[['top', 'bottom', 'right']].set_visible(False)
+            ax10.bar(np.arange(N),prev_weights[0][:],color='aqua')
+            ax10.get_xaxis().set_visible(False)
+            ax10.spines[['top', 'bottom', 'right']].set_visible(False)
 
-            # ax11.bar(np.arange(N),prev_weights[1][:],color='green')
-            # ax11.get_xaxis().set_visible(False)
-            # ax11.spines[['top', 'bottom', 'right']].set_visible(False)
+            ax11.bar(np.arange(N),prev_weights[1][:],color='green')
+            ax11.get_xaxis().set_visible(False)
+            ax11.spines[['top', 'bottom', 'right']].set_visible(False)
 
-            # ax12.bar(np.arange(N),prev_weights[2][:],color='blue')
-            # ax12.get_xaxis().set_visible(False)
-            # ax12.spines[['top', 'bottom', 'right']].set_visible(False)
+            ax12.bar(np.arange(N),prev_weights[2][:],color='blue')
+            ax12.get_xaxis().set_visible(False)
+            ax12.spines[['top', 'bottom', 'right']].set_visible(False)
         
-            # ax13.bar(np.arange(N),prev_weights[3][:],color='purple')
-            # ax13.get_xaxis().set_visible(False)
-            # ax13.spines[['top', 'bottom', 'right']].set_visible(False)
+            ax13.bar(np.arange(N),prev_weights[3][:],color='purple')
+            ax13.get_xaxis().set_visible(False)
+            ax13.spines[['top', 'bottom', 'right']].set_visible(False)
             
-            # ax14.bar(np.arange(N),prev_weights[4][:],color='pink')
-            # ax14.get_xaxis().set_visible(False)
-            # ax14.spines[['top', 'bottom', 'right']].set_visible(False)
+            ax14.bar(np.arange(N),prev_weights[4][:],color='pink')
+            ax14.get_xaxis().set_visible(False)
+            ax14.spines[['top', 'bottom', 'right']].set_visible(False)
 
         ani = FuncAnimation(fig, animate, interval=1,frames=len(velocities),repeat=False)
         plt.show()
@@ -298,23 +298,31 @@ def visualiseMultiResolution1DLandmark(velocities_1rep,scale,visulaise=False):
         ax0.plot(velocities), ax0.set_title('Velocity Profile')#, ax0.axis('equal')
 
         for i in range(0,len(velocities)):
-            if i>=2:
-                input=velocities[i]
+            
+            input=velocities[i]
 
-                delta = [(input/scale[0]), (input/scale[1]), (input/scale[2]), (input/scale[3]), (input/scale[4])]
-                split_output=np.zeros((len(delta)))
-                '''updating network'''    
-                for n in range(len(delta)):
-                    prev_weights[n][:]= net.update_weights_dynamics(prev_weights[n][:],delta[n])
-                    prev_weights[n][prev_weights[n][:]<0]=0
-                    split_output[n]=np.argmax(prev_weights[n][:])  - N//2
-                
-                decoded_translation=np.sum(split_output*scale)
-        
-                integratedPos.append(integratedPos[-1]+input)
-                decodedPos.append(decoded_translation)   
+            delta = [(input/scale[0]), (input/scale[1]), (input/scale[2]), (input/scale[3]), (input/scale[4])]
+            split_output=np.zeros((len(delta)))
+            '''updating network'''    
+            for n in range(len(delta)):
+                prev_weights[n][:]= net.update_weights_dynamics(prev_weights[n][:],delta[n])
+                prev_weights[n][prev_weights[n][:]<0]=0
+                split_output[n]=np.argmax(prev_weights[n][:])  - N//2
 
-                print(f"{str(i)}  translation {input} input output {round(integratedPos[-1],3)}  {str(decoded_translation )}  ")
+            if i < len(velocities_1rep) and i>1 and i%20==0:
+                landmark_storage.append(prev_weights)
+
+            elif i>len(velocities_1rep) and i%20==0:
+                no_lnd=len(velocities_1rep)//20
+                lndmark_id=(i//20)%no_lnd
+                prev_weights+=landmark_storage[lndmark_id]
+            
+            decoded_translation=np.sum(split_output*scale)
+    
+            integratedPos.append(integratedPos[-1]+input)
+            decodedPos.append(decoded_translation)   
+
+            print(f"{str(i)}  translation {input} input output {round(integratedPos[-1],3)}  {str(decoded_translation )} ")
 
         fitness=np.sum(abs(np.array(integratedPos)-np.array(decodedPos)))*-1
         print(fitness)
@@ -442,36 +450,193 @@ def visualiseMultiResolution1DPLotAll(velocities,scale,visulaise=False):
     plt.show()
 
 
+def pathIntegrationVelOnly(rot,velocities,velocity_type):
+    N=100
+    num_links,excite,activity_mag,inhibit_scale=1,3,0.0721745813,2.96673372e-02
+    integratedMag=[0]
+    decodedMag=[0]
+    t=np.arange(len(velocities))
+    x,y=np.zeros(len(velocities)),np.zeros(len(velocities))
+    tru_x,tru_y=np.zeros(len(velocities)),np.zeros(len(velocities))
+    
+    prev_weights=[np.zeros(N), np.zeros(N), np.zeros(N),np.zeros(N), np.zeros(N)]
+    net=attractorNetwork(N,num_links,excite, activity_mag,inhibit_scale)
+    for n in range(len(prev_weights)):
+        prev_weights[n][net.activation(0)]=net.full_weights(num_links)
+
+    fig = plt.figure(figsize=(13, 4))
+    fig_rows,fig_cols=6,3
+    ax0 = plt.subplot2grid(shape=(fig_rows, fig_cols), loc=(0, 0), rowspan=5,colspan=1)
+    ax1 = plt.subplot2grid(shape=(fig_rows, fig_cols), loc=(0, 1), rowspan=5,colspan=1)
+    ax2 = plt.subplot2grid(shape=(fig_rows, fig_cols), loc=(0, 2), rowspan=5,colspan=1)
+    axtxt = plt.subplot2grid(shape=(fig_rows, fig_cols), loc=(5, 0), rowspan=1,colspan=1)
+    fig.tight_layout()
+    ax1.axis('equal'),ax2.axis('equal')
+
+    ax0.plot(velocities), ax0.set_title(f'{velocity_type} Velocity Profile')#, ax0.axis('equal')
+    axtxt.axis('off'), 
+    axtxt.text(0,0,f'Num_links: {num_links}, Excite_radius: {excite}, Activity_magnitude: {activity_mag}, Inhibition_scale: {inhibit_scale}', color='r',fontsize=12)
+
+    for i in range(1,len(velocities)):
+        input=velocities[i]
+
+        delta = [(input/scale[0]), (input/scale[1]), (input/scale[2]), (input/scale[3]), (input/scale[4])]
+        split_output=np.zeros((len(delta)))
+        '''updating network'''    
+        for n in range(len(delta)):
+            prev_weights[n][:]= net.update_weights_dynamics(prev_weights[n][:],delta[n])
+            prev_weights[n][prev_weights[n][:]<0]=0
+            split_output[n]=np.argmax(prev_weights[n][:])
+        
+        decoded_translation=np.sum(split_output*scale)
+
+        integratedMag.append(integratedMag[-1]+input)
+        decodedMag.append(decoded_translation)   
+
+        x[i]=x[i-1]+(decodedMag[i]-decodedMag[i-1])*np.cos(rot[i])
+        y[i]=y[i-1]+(decodedMag[i]-decodedMag[i-1])*np.sin(rot[i])
+
+        tru_x[i]=tru_x[i-1]+input*np.cos(rot[i])
+        tru_y[i]=tru_y[i-1]+input*np.sin(rot[i])
+
+        print(f"{str(i)}  translation {input} input output {round(integratedMag[-1],3)}  {str(decoded_translation )}  ")
+
+    fitness=np.sum(abs(np.array(integratedMag)-np.array(decodedMag)))*-1
+    print(fitness), axtxt.text(0,-1,f'Error: {fitness}', fontsize=12, c='g')
+    ax1.plot(t,integratedMag, t, decodedMag), ax1.set_title('Magnitudes'),ax1.legend(['Ground Truth','Decoded Magnitude'])
+    ax2.plot(tru_x,tru_y,x,y), ax2.set_title('Path Integration'),ax2.legend(['Ground Truth','Integrated Mag&Rot'])
+    plt.show()
+
+def scaleVals(start_idx,ratio):
+    start=[1,1/ratio,1/(ratio**2)]
+    scale=[start[start_idx]]
+    for i in range(4):
+        scale.append(scale[-1]*ratio)
+    return scale
+
+def multiscale_1d_CAN_error(velocities,start_idx, ratio):
+    scale=scaleVals(start_idx, ratio)
+    # print(start_idx, ratio,scale)
+    N=100
+    num_links,excite,activity_mag,inhibit_scale=1,3,0.0721745813,2.96673372e-02
+    integratedMag=[0]
+    decodedMag=[0]
+
+    prev_weights=[np.zeros(N), np.zeros(N), np.zeros(N),np.zeros(N), np.zeros(N)]
+    net=attractorNetwork(N,num_links,excite, activity_mag,inhibit_scale)
+    for n in range(len(prev_weights)):
+        prev_weights[n][net.activation(0)]=net.full_weights(num_links)
+    
+    for i in range(1,len(velocities)):
+        input=velocities[i]
+
+        delta = [(input/scale[0]), (input/scale[1]), (input/scale[2]), (input/scale[3]), (input/scale[4])]
+        split_output=np.zeros((len(delta)))
+        '''updating network'''    
+        for n in range(len(delta)):
+            prev_weights[n][:]= net.update_weights_dynamics(prev_weights[n][:],delta[n])
+            prev_weights[n][prev_weights[n][:]<0]=0
+            split_output[n]=np.argmax(prev_weights[n][:])
+        
+        decoded_translation=np.sum(split_output*scale)
+
+        integratedMag.append(integratedMag[-1]+input)
+        decodedMag.append(decoded_translation)   
+
+    fitness=np.sum(abs(np.array(integratedMag)-np.array(decodedMag)))*-1
+    return fitness
+
+
+def gridSearch(filename,error_func,velocities):
+    start_idxs=[0,1,2]
+    ratios=list(np.arange(2,11))
+    error=np.zeros((len(start_idxs),len(ratios)))
+    for i,stid in enumerate(start_idxs):
+        for j,rat in enumerate(ratios):
+            # print(mag,inh)
+            t=time.time()
+            error[i,j]=error_func(velocities,stid, rat)
+            print(i,j,error[i,j], time.time()-t)
+    with open(filename, 'wb') as f:
+        np.save(f, np.array(error))
+
+def plottingGridSearch(filename):
+    start_idxs=[0,1,2]
+    ratios=list(np.arange(2,11))
+    with open(filename, 'rb') as f:
+        error = np.load(f)
+        norm_error=error/np.linalg.norm(error)
+
+    fig, ax0=plt.subplots(figsize=(10, 7), ncols=1)
+    print(error)
+    ax0.set_title(filename)
+    pos= ax0.imshow(error)
+    fig.colorbar(pos)
+    ax0.set_ylabel('Start Value')
+    ax0.set_xlabel('Ratio')
+    ax0.set_xticks(np.arange(len(ratios)),[round(a,4) for a in ratios])
+    ax0.set_yticks(np.arange(len(start_idxs)), [1,'1/ratio','1/ratio^2'],rotation=90)
+    # ax0.grid(True)
+
+    plt.show()
+
+
+def data_processing_groundTruth():
+    poses = pd.read_csv('./data/dataset/poses/00.txt', delimiter=' ', header=None)
+    gt = np.zeros((len(poses), 3, 4))
+    for i in range(len(poses)):
+        gt[i] = np.array(poses.iloc[i]).reshape((3, 4))
+    gt=gt[0::4]
+    data_x=gt[:, :, 3][:,0][:100]
+    data_y=gt[:, :, 3][:,2][:100]
+    mag,rot=np.zeros(len(data_x)),np.zeros(len(data_x))
+    for i in range(1,len(data_x)):  
+        x1=data_x[i-1]
+        x2=data_x[i]
+        y1=data_y[i-1]
+        y2=data_y[i]
+        mag[i]=np.sqrt(((x2-x1)**2)+((y2-y1)**2))
+        rot[i]=(math.atan2(y2-y1,x2-x1)) 
+    return gt,mag,rot
+
+def data_processing_oxts():
+    path='./data/2011_09_26_2/2011_09_26_drive_0001_sync/oxts/data/'
+    filenames = [f for f in listdir(path)]
+    filenames.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
+    '''
+    vn:    velocity towards north (m/s)
+    ve:    velocity towards east (m/s)
+    vf:    forward velocity, i.e. parallel to earth-surface (m/s)
+    vl:    leftward velocity, i.e. parallel to earth-surface (m/s)
+    vu:    upward velocity, i.e. perpendicular to earth-surface (m/s)'''
+    vn,ve,vf,vl,vu=np.zeros(len(filenames)), np.zeros(len(filenames)), np.zeros(len(filenames)), np.zeros(len(filenames)), np.zeros(len(filenames))
+    for i in range(len(filenames)):
+        cur_file=pd.read_csv(path+filenames[i], delimiter=' ', header=None)
+        vn[i]=cur_file[7]
+        ve[i]=cur_file[8]
+        vf[i]=cur_file[9]
+        vl[i]=cur_file[10]
+        vu[i]=cur_file[11]
+    return vn,ve,vf,vl,vu
+
+
 '''test'''
+gt,mag,rot=data_processing_groundTruth()
+vn,ve,vf,vl,vu=data_processing_oxts()
+scale=[0.5,1,2,4,8]
+
 # increasing 
 velocities=np.concatenate([np.array([0.01]*25), np.zeros(25), np.array([0.1]*25), np.zeros(25), np.array([1]*25), np.zeros(25), np.array([10]*25), np.zeros(25), np.array([100]*25)])
 # velocities increasing then decreainf 
 velocities=np.concatenate([np.array([100]*25), np.array([10]*25), np.array([1]*25), np.array([0.1]*25), np.array([0.01]*25), np.array([0.1]*25),  np.array([1]*25),  np.array([10]*25),  np.array([100]*25)])
 # random normal distribution 
-velocities=np.concatenate([np.random.uniform(100,10,20),np.random.uniform(10,1,20), np.random.uniform(1,0.1,20), np.random.uniform(0.1,0.01,20), np.random.uniform(0.01,0.001,21)])
-# velocities=np.random.normal(1,0.5,50)
-scale=[0.01,0.1,1,10,100]
-path='./data/2011_09_26_2/2011_09_26_drive_0001_sync/oxts/data/'
-filenames = [f for f in listdir(path)]
-filenames.sort(key=lambda f: int(''.join(filter(str.isdigit, f))))
-'''
-vn:    velocity towards north (m/s)
-ve:    velocity towards east (m/s)
-vf:    forward velocity, i.e. parallel to earth-surface (m/s)
-vl:    leftward velocity, i.e. parallel to earth-surface (m/s)
-vu:    upward velocity, i.e. perpendicular to earth-surface (m/s)'''
-vn,ve,vf,vl,vu=np.zeros(len(filenames)), np.zeros(len(filenames)), np.zeros(len(filenames)), np.zeros(len(filenames)), np.zeros(len(filenames))
-for i in range(len(filenames)):
-    cur_file=pd.read_csv(path+filenames[i], delimiter=' ', header=None)
-    vn[i]=cur_file[7]
-    ve[i]=cur_file[8]
-    vf[i]=cur_file[9]
-    vl[i]=cur_file[10]
-    vu[i]=cur_file[11]
-# print(vf)
-# print(vl)
+velocities=np.concatenate([np.random.uniform(0,10,20), np.random.uniform(0,1,20), np.random.uniform(0,0.1,21)])
 
+filename=f'./results/AblationStudyScales/10scales_3starts.npy'
+# gridSearch(filename,multiscale_1d_CAN_error,velocities)
+# plottingGridSearch(filename)
 
-visualiseMultiResolution1D(velocities,scale,'Curated',visualise=True)
-# visualiseMultiResolution1DLandmark(velocities,scale,visulaise=True)
+pathIntegrationVelOnly(rot,mag,'KittiDataset')
+# visualiseMultiResolution1D(mag,scale,'Curated',visualise=False)
+# visualiseMultiResolution1DLandmark(velocities,scale,visulaise=False)
 # visualiseMultiResolution1DPLotAll(velocities,scale,visulaise=True)
